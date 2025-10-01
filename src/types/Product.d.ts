f1d47a665e1c7afdf26bea36fe11b45baf3ddfb7
Andika Syamsiana <andikayamana14045@gmail.com>
2025-10-01 22:07:32 +0700

    fix: make all fields in IProduct interface required

--- src/types/Product.d.ts
+++ src/types/Product.d.ts
@@ -1,19 +1,19 @@
 interface IProduct {
   id: number;
-  image?: string;
-  title?: string;
-  desc?: string;
-  category?: string[];
+  image: string;
+  title: string;
+  desc: string;
+  category: string[];
 
-  author?: string;
-  authorImage?: string;
-  authorTitle?: string;
-  aurhorCompany?: string;
+  author: string;
+  authorImage: string;
+  authorTitle: string;
+  aurhorCompany: string;
 
-  rating?: number;
-  review?: number;
-  price?: number;
-  discount?: number;
+  rating: number;
+  review: number;
+  price: number;
+  discount: number;
 }
 
 export type { IProduct };
